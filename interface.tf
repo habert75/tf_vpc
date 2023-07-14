2b95e033b32096b0d7ed3f2f36bff0400c78e033
Jean <habertbertrando7@gmail.com>
2023-07-14 17:20:03 +0200

    fixing input variables bug

--- interface.tf
+++ interface.tf
@@ -1,31 +1,36 @@
 variable "name" {
-type = string
-description = "The name of the VPC."
+  type        = string
+  description = "The name of the VPC."
 }
 variable "cidr" {
-type = string
-description = "The CIDR of the VPC."
+  type        = string
+  description = "The CIDR of the VPC."
 }
 variable "public_subnet" {
-type = string
-description = "The public subnet to create."
+  type        = string
+  description = "The public subnet to create."
 }
 variable "enable_dns_hostnames" {
-type = bool
-description = "Should be true if you want to use private DNS within the VPC"
-default = true
+  type        = bool
+  description = "Should be true if you want to use private DNS within the VPC"
+  default     = true
 }
 variable "enable_dns_support" {
-type = bool
-description = "Should be true if you want to use private DNS within the VPC"
-default = true
+  type        = bool
+  description = "Should be true if you want to use private DNS within the VPC"
+  default     = true
+}
+variable "map_public_ip_on_launch" {
+  type        = map(string)
+  description = "A map"
+  default     = {}
 }
 output "public_subnet_id" {
-value = aws_subnet.public.id
+  value = aws_subnet.public.id
 }
 output "vpc_id" {
-value = aws_vpc.tfb.id
+  value = aws_vpc.tfb.id
 }
 output "cidr" {
-value = aws_vpc.tfb.cidr_block
+  value = aws_vpc.tfb.cidr_block
 }
